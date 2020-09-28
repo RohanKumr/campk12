41d8cd71539016ad5ff88526f76a796ad7ab4e2b
Rohan Kumar <rkrohanrk065@gmail.com>
2020-09-28 19:05:05 +0530

    Updates

---
+++ precache-manifest.c728769882c0ea2a39caf5f1301afb1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e283893a9fab3a5700cba98ee93b6515",
+    "url": "/campk12/index.html"
+  },
+  {
+    "revision": "8f679b3bd3666dab69ed",
+    "url": "/campk12/static/css/2.d8c3f0bf.chunk.css"
+  },
+  {
+    "revision": "27c586dc767d1beb78fe",
+    "url": "/campk12/static/css/main.acd47882.chunk.css"
+  },
+  {
+    "revision": "8f679b3bd3666dab69ed",
+    "url": "/campk12/static/js/2.4436e1e9.chunk.js"
+  },
+  {
+    "revision": "ae64ee99a9d1943a09c72197d1a55f1a",
+    "url": "/campk12/static/js/2.4436e1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c586dc767d1beb78fe",
+    "url": "/campk12/static/js/main.0a6284e1.chunk.js"
+  },
+  {
+    "revision": "6323c128c90c38ea9f22",
+    "url": "/campk12/static/js/runtime-main.daac7c88.js"
+  }
+]);

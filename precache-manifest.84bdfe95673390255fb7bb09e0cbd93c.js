df1d4aaad3c2eba47697a8ea8d498a4319359628
Rohan Kumar <rkrohanrk065@gmail.com>
2020-09-29 15:58:53 +0530

    Updates

--- precache-manifest.84bdfe95673390255fb7bb09e0cbd93c.js
+++ precache-manifest.84bdfe95673390255fb7bb09e0cbd93c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b697b5c882ba80cdf45b512334d428d5",
+    "revision": "25c121b0ef34b1f17aaa1559a26f4519",
     "url": "/campk12/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/campk12/static/css/2.d8c3f0bf.chunk.css"
   },
   {
-    "revision": "d8253573c387eeffcbb9",
-    "url": "/campk12/static/css/main.22382644.chunk.css"
+    "revision": "b793d1ca5918a329720d",
+    "url": "/campk12/static/css/main.8fcc7378.chunk.css"
   },
   {
     "revision": "5807e43e172a173bcf5e",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/campk12/static/js/2.48f86a6a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d8253573c387eeffcbb9",
+    "revision": "b793d1ca5918a329720d",
     "url": "/campk12/static/js/main.bd0f99d1.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/campk12/precache-manifest.d68f0ff1880f10449464af14c681ea1d.js"
+  "/campk12/precache-manifest.84bdfe95673390255fb7bb09e0cbd93c.js"
 );
 
 self.addEventListener('message', (event) => {
